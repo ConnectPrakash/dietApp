49b39537484e3e7a99505e0efa8375232d0de779
ConnectPrakash <pp3662504@gmail.com>
2024-03-31 21:24:40 +0530

    diet website

---
+++ src/component/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+       <section className='home' id='home'>
+
+<div className='content'>
+  <h3>Welcome to the place full of healthy food</h3>
+  <a href='#' className='btn'>read more</a>
+</div>
+</section>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/script.js
@@ -0,0 +1,48 @@
+let navbar = document.querySelector('.header .navbar');
+let menu = document.querySelector('#menu');
+
+menu.onClick = () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+};
+
+window.onscroll =() =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+
+$(document).ready(function(){
+
+    $('.buttons').click(function(){
+ 
+        $(this).addClass('active').siblings().removeClass('active');
+
+        var filter = $(this).attr('data-filter')
+
+        if(filter == 'all'){
+            $('.diet .box').show(400);
+        }
+        else{
+            $('.diet .box').not('.'+filter).hide(200);
+            $('.diet .box').filter('.'+filter).show(400);
+        }
+    })
+});
+
+var swiper = new swiper('.review-slider',{
+    loop:true,
+    grabCursor:true,
+    spaceBetween:20,
+    breakpoints:{
+        0:{
+            slidesPerview:1,
+        },
+        640:{
+            slidesPerview:2,
+        },
+        768:{
+            slidesPerview:3,
+        }
+    }
+})
